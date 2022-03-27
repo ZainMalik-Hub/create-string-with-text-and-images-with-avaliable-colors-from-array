7b9ff1c9c99d631134c0eeaed519cd444101c535
zainmalik1 <mzain6805@gmail.com>
2022-03-28 02:09:49 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Text from "../src/components/pages/TextPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Text />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/TextPage.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import bulb from "../../assets/bulb.svg";
+import underlineWind from "../../assets/underlineWind.svg";
+import boom from "../../assets/boom.svg";
+import rainbow from "../../assets/rainbow.svg";
+import atom from "../../assets/atom.svg";
+import underlineSawtooth from "../../assets/underlineSawtooth.svg";
+import smiley from "../../assets/smiley.svg";
+import underlineWaves from "../../assets/underlineWaves.svg";
+
+function Text() {
+  const [notes, setnotes] = useState();
+  const [dText, setDtext] = useState();
+  const url = "https://www.entwurf.co.uk/api/code-test/7439028";
+  useEffect(() => {
+    getTextApi();
+  }, []);
+
+  const getTextApi = () => {
+    axios
+      .get(url)
+      .then((response) => {
+        setnotes(response?.data);
+      })
+      .catch((error) => console.error(`Error: ${error} `));
+  };
+  //   console.log("response", notes?.data?.keywordList);
+  let text = notes?.data?.sectionTextCopy;
+  const keywords = notes?.data?.keywordList;
+
+  notes?.data?.keywordList.forEach((v) => {
+    if (text.includes(v.keyword)) {
+      var hh = "";
+      if (v.svg === "bulb") {
+        hh = bulb;
+      }
+      if (v.svg === "underlineWind") {
+        hh = underlineWind;
+      }
+      if (v.svg === "boom") {
+        hh = boom;
+      }
+      if (v.svg === "rainbow") {
+        hh = rainbow;
+      }
+      if (v.svg === "atom") {
+        hh = atom;
+      }
+      if (v.svg === "underlineSawtooth") {
+        hh = underlineSawtooth;
+      }
+      if (v.svg === "smiley") {
+        hh = smiley;
+      }
+      if (v.svg === "underlineWaves") {
+        hh = underlineWaves;
+      }
+
+      text = text.replace(
+        v.keyword,
+        `<span style=' color:${v.color}'>${
+          v.keyword
+        }<img style='width:${"26px"}'  src=${hh}> </span>`
+      );
+    }
+  });
+
+  return (
+    <div style={{ backgroundColor: "blue", display: "flex" }}>
+      <div
+        style={{ fontSize: "30px", width: "400px" }}
+        dangerouslySetInnerHTML={{ __html: `${text}` }}
+      ></div>
+    </div>
+  );
+}
+export default Text;
